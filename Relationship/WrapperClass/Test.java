2159168fcbd92df127e47cde28ce277def9167cd
Manggeshpatil <166353759+Manggeshpatil@users.noreply.github.com>
2026-04-23 15:45:39 +0530

    Add files via upload

---
+++ Relationship/WrapperClass/Test.java
@@ -0,0 +1,11 @@
+package WrapperClass;
+
+public class Test {
+ 
+public static void main(String[] args) {
+	String s1="10"; 
+	System.out.println(s1);
+	int a1 = Integer.parseInt(s1);
+	System.out.println(a1);
+}
+}
---
+++ Relationship/WrapperClass/Test1.java
@@ -0,0 +1,11 @@
+package WrapperClass;
+
+public class Test1 {
+	public static void main(String[] args) {
+		
+	String s1="True"; 
+	System.out.println(s1);
+	boolean a1 = Boolean.parseBoolean(s1);
+	System.out.println(a1);
+	}
+}
